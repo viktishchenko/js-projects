ca30413eb423e63d57d56e01cf8536549d911c3e
Viktor Tishchenko <86599419+viktishchenko@users.noreply.github.com>
2025-04-17 09:50:15 +0600

    Update app.js placeholder img

--- get-data/app.js
+++ get-data/app.js
@@ -5,7 +5,7 @@ const menu = [
     title: "buttermilk pancakes",
     category: "breakfast",
     price: 15.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
@@ -13,7 +13,7 @@ const menu = [
     title: "diner double",
     category: "lunch",
     price: 13.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
@@ -21,7 +21,7 @@ const menu = [
     title: "godzilla milkshake",
     category: "shakes",
     price: 6.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
@@ -29,7 +29,7 @@ const menu = [
     title: "country delight",
     category: "breakfast",
     price: 20.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
@@ -37,7 +37,7 @@ const menu = [
     title: "egg attack",
     category: "lunch",
     price: 22.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
   {
@@ -45,7 +45,7 @@ const menu = [
     title: "oreo dream",
     category: "shakes",
     price: 18.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
@@ -53,7 +53,7 @@ const menu = [
     title: "bacon overflow",
     category: "breakfast",
     price: 8.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
   {
@@ -61,7 +61,7 @@ const menu = [
     title: "american classic",
     category: "lunch",
     price: 12.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
   {
@@ -69,7 +69,7 @@ const menu = [
     title: "quarantine buddy",
     category: "shakes",
     price: 16.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
   {
@@ -77,7 +77,7 @@ const menu = [
     title: "bison steak",
     category: "dinner",
     price: 22.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
   {
@@ -85,7 +85,7 @@ const menu = [
     title: "bison steak",
     category: "shmule",
     price: 50.99,
-    img: "https://via.placeholder.com/450x300",
+    img: "https://placehold.co/450x300/png",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
